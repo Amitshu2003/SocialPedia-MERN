547d429e9f735048bcde45da87a6b5f38d4c3d00
Amitshu2003 <201951021@iiitvadodara.ac.in>
2023-08-12 09:52:50 +0530

    add live url in forntend

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,22 +1,6 @@
-// import axios from 'axios'
-
-// const url = 'http://localhost:5000/posts'
-
-// export const fetchPosts = ()=> axios.get(url)
-// export const createPost = (newPost) => axios.post(url,newPost)
-// export const updatePost = (id, updatedPost) => axios.put(`${url}/${id}`,updatedPost)
-// export const deletePost = (id) => axios.delete(`${url}/${id}`)
-// export const likePost =  (id) => axios.put(`${url}/${id}/likePost`)
-
-// const authUrl = 'http://localhost:5000/users'
-
-// export const signIn = (formData)=> axios.post(`${authUrl}/signin`,formData)
-// export const signUp = (formData)=> axios.post(`${authUrl}/signup`,formData)
-
-
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({ baseURL: 'https://socialpedia-backend-ifep.onrender.com' });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
